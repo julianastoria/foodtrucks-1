62da91d46a74a5ff00fbd6ef1813b6fde722dad3
Arnaud <contact@osw3.net>
2017-07-24 09:40:23 +0200

    initialisation du projet

--- private/views/register.php
+++ private/views/register.php
@@ -1,1 +1,118 @@
-Page Inscription
+<?php
+
+// Definition des variables par défaut
+$login       = null;
+$password    = null;
+$firstname   = null;
+$lastname    = null;
+$gender      = null;
+$birth_day   = null;
+$birth_month = null;
+$birth_year  = null;
+
+// Cas où l'utilisateur envoie le formulaire (méthode POST)
+// Contrôle du formulaire
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+}
+
+// Cas où l'utilisateur arrive sur la page sans envoyer le formulaire (méthode GET)
+else {
+
+}
+?>
+
+<div class="page-header">
+    <h2>Inscription</h2>
+</div>
+
+<div class="row">
+    <div class="col-md-4 col-md-offset-4">
+
+        <?php getFlashbag(); ?>
+
+        <form method="post">
+
+            <input type="text" name="token" value="<?php echo $_SESSION['token']; ?>">
+
+            <div class="form-group">
+                <label for="login">Identifiant (adresse email)</label>
+                <input  class="form-control" type="text" id="login" name="login" value="<?php echo $login; ?>">
+            </div>
+
+            <div class="form-group">
+                <label for="password">Mot de passe</label>
+                <input  class="form-control" type="password" id="password" name="password">
+            </div>
+
+            <div class="form-group">
+                <label for="firstname">Prénom</label>
+                <input  class="form-control" type="text" id="firstname" name="firstname" value="<?php echo $firstname; ?>">
+            </div>
+
+            <div class="form-group">
+                <label for="lastname">Nom de famille</label>
+                <input  class="form-control" type="text" id="lastname" name="lastname" value="<?php echo $lastname; ?>">
+            </div>
+
+            <div class="form-group">
+                <label>Genre</label>
+                <label><input type="radio" name="gender" value="F" <?php echo $gender == "F" ? "checked" : null; ?>> Féminin </label>
+                <label><input type="radio" name="gender" value="M" <?php echo $gender == "M" ? "checked" : null; ?>> Masculin </label>
+                <label><input type="radio" name="gender" value="T" <?php echo $gender == "T" ? "checked" : null; ?>> Trans </label>
+                <label><input type="radio" name="gender" value="A" <?php echo $gender == "A" ? "checked" : null; ?>> Alien </label>
+            </div>
+
+            <div class="form-group">
+                <label for="birthday">Date de naissance</label>
+                <div class="row">
+                    <div class="col-md-4">
+                        <select  class="form-control" id="birthday" name="birth[day]">
+                            <option value="">Jour</option>
+                            <?php for($i=1; $i<=31; $i++): ?>
+                                <option value="<?php echo str_pad($i, 2, 0, STR_PAD_LEFT); ?>"><?php
+                                    echo str_pad($i, 2, 0, STR_PAD_LEFT);
+                                ?></option>
+                            <?php endfor; ?>
+                        </select>
+                    </div>
+
+                    <div class="col-md-4">
+                        <select  class="form-control" name="birth[month]">
+                            <option value="">Mois</option>
+                            <?php for($i=0; $i<12; $i++): ?>
+                                <option value="<?php echo str_pad(($i+1), 2, 0, STR_PAD_LEFT); ?>"><?php echo $month[$i]; ?></option>
+                            <?php endfor; ?>
+                        </select>
+                    </div>
+
+                    <div class="col-md-4">
+                        <select  class="form-control" name="birth[year]">
+                            <option value="">Années</option>
+                            <?php for($i=date('Y'); $i>date('Y')-100; $i--): ?>
+                                <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                            <?php endfor; ?>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <label>
+                    <input type="checkbox" name="acceptTerms">
+                    J'accepte les conditions d'utilisation du service.
+                </label>
+
+            </div>
+
+            <br>
+            <button type="submit" class="btn btn-info btn-block">Valider</button>
+        </form>
+
+        <p class="text-center">
+            <a href="index.php?page=login">J'ai déjà compte</a>
+        </p>
+
+
+    </div>
+</div>
